c72a69503e1e1e25d40dfe01e6e274a673789fda
kassahunl <kassahunketema@gmail.com>
2022-06-02 17:30:31 -0700

    all the mandatory tasks for C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- * main - Prints a simple statments
+ * main - prints a simple statment
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"programming is like building a multilingual puzzle\n");
 	return (0);
 }
