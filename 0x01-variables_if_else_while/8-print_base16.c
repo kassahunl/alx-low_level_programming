3dd303cc1d2010c0e6297db4c006d1bab4e6f80c
kassahunl <kassahunketema@gmail.com>
2022-06-03 19:33:56 -0700

    task 8 of C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - prints single digit base 16 numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
